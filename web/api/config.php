48bc82900dbf77f586a2a1bd4d27b2eb5a14f809
Jason D. McCormick <jason@mfamily.org>
2023-03-03 15:55:24 -0500

    add initial config and functions

---
+++ web/api/config.php
@@ -0,0 +1,24 @@
+<?php
+#
+# Configuration for the Web API of Allmon3
+# Note: This is PHP code executed inline of the program
+# so proper PHP syntax is required
+#
+
+# Location of allmon3.ini
+$CONFIG_ALLMON3_INI = "/usr/local/etc/allmon3.ini";
+
+
+
+#####                                       #####
+##### WARNING - THIS ARE INTERNAL VARIABLES #####
+##### IT IS **EXTREMELY** UNLIKELY YOU EVER #####
+##### NEED TO CHANGE ANYTHING BELOW!!       #####
+#####                                       #####
+
+$CONFIG_ZMQ_LOCALHOST = "127.0.0.1";
+$CONFIG_ZMQ_SNDTIMEO = 2000;
+$CONFIG_ZMQ_RCVTIMEO = 2000;
+$CONFIG_ZMQ_LINGER = 2000;
+$CONFIG_ZMQ_SUBSCRIBE = "";
+?>
---
+++ web/api/functions.php
@@ -0,0 +1,29 @@
+<?php
+#
+# Common functions for all Allmon3 web API
+#
+
+function getGetVar($id) {
+    return filter_var(trim($_GET[$id]), FILTER_SANITIZE_STRING);
+}
+
+function genJSONError($errmsg) {
+	return sprintf("{ \"%s\" : \"%s\" }", "ERROR", $errmsg);
+}
+
+# Return the value from an .ini-sourced file
+# $ini - INI Array, $section - [SECTION], $key = field
+# returns the value or false
+function getINIConfigVal($ini, $section, $key){
+	if( ! array_key_exists($section, $ini)){
+		return false;
+	} else {
+		if( array_key_exists($key, $ini[$section])){
+			return $ini[$section][$key];
+		} else
+			return false;
+		}
+}
+
+
+?>
