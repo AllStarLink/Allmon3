a19aba5839dcd7aeef9e97d8daa0102277a35a75
Jason D. McCormick <jason@mfamily.org>
2023-03-03 12:34:11 -0500

    be useful

--- asl-statmon/asl-statmon-test-client.py
+++ asl-statmon/asl-statmon-test-client.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
+#
+# Simple client to see that an asl-statmon instance 
+# is working properly
 #
 
+import argparse
+import signal
+import sys
 import zmq
 
-context = zmq.Context()
+def sigterm_handler(_signo, _stack_frame):
+	print("exiting on signal %d" % (_signo))
+	socket.close()
+	sys.exit(0)
 
-#  Socket to talk to server
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:6750")
-socket.setsockopt(zmq.SUBSCRIBE, b'')
+signal.signal(signal.SIGINT, sigterm_handler)
+signal.signal(signal.SIGHUP, sigterm_handler)
+signal.signal(signal.SIGTERM, sigterm_handler)
 
-while True:
-    message = socket.recv()
-    print("%s" % message.decode("UTF-8"))
+ap = argparse.ArgumentParser(description="Simple client to see that an asl-statmon instance. Dumps 0MQ messages from the specified port")
+ap.add_argument("host", type=str, help="FQDN or IP address of the asl-statmon to test")
+ap.add_argument("port", type=int, help="TCP port of asl-statmon to test")
+args = ap.parse_args()
+
+try:
+	print("Creating 0MQ connection to %s:%d" % (args.host,args.port))
+	context = zmq.Context()
+	socket = context.socket(zmq.SUB)
+	socket.connect("tcp://%s:%d" % (args.host, args.port))
+	print("Connected. Press CTRL+c to stop...")
+	socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+	while True:
+	    message = socket.recv()
+	    print("%s" % message.decode("UTF-8"))
+
+except Exception as e:
+	print("Error: " + e)
+	system.exit(1)
