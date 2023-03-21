39ad2423ffd17d06ca23a109900d5759cdaa2417
Jason D. McCormick <jason@mfamily.org>
2023-03-20 20:42:28 -0400

    forgot to add under logon work

---
+++ web/api/session-check.php
@@ -0,0 +1,19 @@
+<?php
+#
+# Copyright(C) 2023 AllStarLink
+# Allmon3 and all components are Licensed under the AGPLv3
+# see https://raw.githubusercontent.com/AllStarLink/Allmon3/develop/LICENSE
+#
+
+require_once("functions.php");
+require_once("config.php");
+require_once("session-handler.php");
+
+if(strcmp(php_sapi_name(),"cli") != 0){
+	header('Content-Type: application/json');
+}
+
+print getJSONSuccess("OK");
+exit(0);
+
+?>
