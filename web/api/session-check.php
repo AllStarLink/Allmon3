6cf385d7003e308ad862808911f818a59d660039
Jason D. McCormick <jason@mfamily.org>
2023-04-08 13:40:22 -0400

    relocate config.php to /etc/allmon3

--- web/api/session-check.php
+++ web/api/session-check.php
@@ -6,7 +6,7 @@
 #
 
 require_once("functions.php");
-require_once("config.php");
+require_once("/etc/allmon3/config.php");
 require_once("session-handler.php");
 
 if(strcmp(php_sapi_name(),"cli") != 0){
