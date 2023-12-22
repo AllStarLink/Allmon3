336597b5c94c33e165ba5aca3c8275a50b3437c1
Jason D. McCormick <jason@mfamily.org>
2023-12-22 11:27:47 -0500

    x

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -65,6 +65,8 @@ echo "Operating Systems: $OPERATING_SYSTEMS"
 echo "PWD: $(pwd)"
 echo `find . -print -maxdepth 1`
 
+echo "BS: ${BASH_SOURCE[0]}"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: ${DIR}"
 PDIR=$(dirname $DIR)
