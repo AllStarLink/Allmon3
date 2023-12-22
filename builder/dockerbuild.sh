015b22f490441e792bca004a4bb6eeea70f57501
Jason D. McCormick <jason@mfamily.org>
2023-12-22 17:14:48 -0500

    basic framework

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -64,10 +64,7 @@ echo "Architectures: $ARCH"
 echo "Targets: $TARGETS"
 echo "Operating Systems: $OPERATING_SYSTEMS"
 echo "PWD: $(pwd)"
-echo `find . -print -maxdepth 1`
-
 echo "BS: ${BASH_SOURCE[0]}"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: ${DIR}"
 PDIR=$(dirname $DIR)
@@ -76,17 +73,17 @@ echo "PDIR: ${PDIR}"
 DPKG_BUILDOPTS="-b -uc -us"
 D_TAG="allmon3_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
 
-docker build -f $DIR/Dockerfile -t $D_TAG \
-	--build-arg ARCH="$ARCH" \
-	--build-arg OS="$OPERATING_SYSTEMS" \
-	--build-arg ASL_REPO="asl_builds${REPO_ENV}" \ 
-	--build-arg USER_ID=$(id -u) \
-	--build-arg GROUP_ID=$(id -g) \
-	$DIR
+#docker build -f $DIR/Dockerfile -t $D_TAG \
+#	--build-arg ARCH="$ARCH" \
+#	--build-arg OS="$OPERATING_SYSTEMS" \
+#	--build-arg ASL_REPO="asl_builds${REPO_ENV}" \
+#	--build-arg USER_ID=$(id -u) \
+#	--build-arg GROUP_ID=$(id -g) \
+#	$DIR
 
 docker run -v $PDIR:/src \
 	-e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" \
-	-e BUILD_TARGETS="$BUILD_TARGETS" \
+	-e BUILD_TARGETS="$TARGETS" \
 	$D_TAG
 
-docker image rm --force $D_TAG
+#docker image rm --force $D_TAG
