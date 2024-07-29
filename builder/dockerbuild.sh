b152f56bf1d9611964280599156c438bf2118780
Jason D. McCormick <jason@mfamily.org>
2024-07-28 20:30:22 -0400

    ASL3 version

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -5,22 +5,9 @@ set -x
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -c|--check-changelog)
-      CHECK_CHANGELOG=YES
-      shift
-      ;;
     -a|--architecture)
-      ARCH="$2"
+      ARCH_ASK="$2"
       shift
-      shift
-      ;;
-    -t|--targets)
-      TARGETS="$2"
-      shift
-      shift
-      ;;
-    -r|--commit-versioning)
-      COMMIT_VERSIONING=YES
       shift
       ;;
     -o|--operating-systems)
@@ -28,26 +15,44 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
-    -*|--*|*)
+    --gh-rel)
+      GH_REL="$2"
+      shift
+      shift
+      ;;
+	-r)
+      APTLY_REPO="asl3-$2"
+      shift
+      shift	
+      ;;
+	-*|--*|*)
       echo "Unknown option $1"
-      exit 1
+	  shift
+      shift
       ;;
   esac
 done
 
-if [ -z "$ARCH" ]
+if [ -z "$ARCH_ASK" ]
 then
   ARCH="all"
 fi
 
-if [ -z "$TARGETS" ]
-then
-  TARGETS="Allmon3"
-fi
+case $ARCH_ASK in
+	arm64)
+		ARCH=arm64v8
+		;;
+	armhf)
+		ARCH=arm32v7
+		;;
+	*)
+		ARCH=$ARCH_ASK
+		;;
+esac
 
 if [ -z "$OPERATING_SYSTEMS" ]
 then
-  OPERATING_SYSTEMS="buster"
+  OPERATING_SYSTEMS="bookworm"
 fi
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -60,6 +65,7 @@ else
   REPO_ENV=""
 fi
 
+## Need to clean this up to be more elegant
 echo "Architectures: $ARCH"
 echo "Targets: $TARGETS"
 echo "Operating Systems: $OPERATING_SYSTEMS"
@@ -69,21 +75,37 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: ${DIR}"
 PDIR=$(dirname $DIR)
 echo "PDIR: ${PDIR}"
+ALL_PKG_ROOT=$(dirname ${PDIR})
+echo "ALL_PKG_ROOT: ${ALL_PKG_ROOT}"
+echo "GH_REL: ${GH_REL}"
 
-DPKG_BUILDOPTS="-b -uc -us"
-D_TAG="allmon3_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
+D_TAG="Allmon3_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
 
 docker build -f $DIR/Dockerfile -t $D_TAG \
 	--build-arg ARCH="$ARCH" \
 	--build-arg OS="$OPERATING_SYSTEMS" \
-	--build-arg ASL_REPO="asl_builds${REPO_ENV}" \
 	--build-arg USER_ID=$(id -u) \
 	--build-arg GROUP_ID=$(id -g) \
 	$DIR
+ 
+docker run -v $ALL_PKG_ROOT:/build $D_TAG
 
-docker run -v $PDIR:/build/Allmon3 \
-	-e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" \
-	-e BUILD_TARGETS="$TARGETS" \
-	$D_TAG
+DEBIAN_FRONTEND=noninteractive apt-get -y install gh
+gh release upload -R AllStarLink/Allmon3 $GH_REL $ALL_PKG_ROOT/_debs/*.deb
 
 docker image rm --force $D_TAG
+
+APTLY_USER="${APTLY_API_USER}:${APTLY_API_PASS}"
+
+find $ALL_PKG_ROOT/_debs/*.deb -name "*.deb" | \
+	xargs -I {} -d '\n' curl --fail --user ${APTLY_USER} \
+	-X POST -F 'file=@"{}"' \
+	 https://repo-admin.allstarlink.org/api/files/${APTLY_REPO}-${OPERATING_SYSTEMS}
+
+curl --fail --user ${APTLY_USER} -X POST \
+	https://repo-admin.allstarlink.org/api/repos/${APTLY_REPO}/file/${APTLY_REPO}-${OPERATING_SYSTEMS}
+
+curl --fail --user ${APTLY_USER} -X PUT -H "content-Type: application/json" \
+	--data "{\"Signing\": {\"Batch\": true, \"Passphrase\": \"${APTLY_GPG_PASSPHRASE}\"}}" \
+	"https://repo-admin.allstarlink.org/api/publish/:./${OPERATING_SYSTEMS}"
+
